d6e38d793e58aaaefcbd058817311bedc0a3268e
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-18 17:42:43 +0530

    Update AlitaTraining/src/main/java/org/epam/learn/stepdefinition/LoginSteps.java

--- AlitaTraining/src/main/java/org/epam/learn/stepdefinition/LoginSteps.java
+++ AlitaTraining/src/main/java/org/epam/learn/stepdefinition/LoginSteps.java
@@ -1,4 +1,4 @@
-package com.saucedemo.steps;
+package com.saucedemo.stepdefinitions;
 
 import com.saucedemo.pages.LoginPage;
 import io.cucumber.java.en.Given;
@@ -13,72 +13,83 @@ public class LoginSteps {
     private WebDriver driver;
     private LoginPage loginPage;
 
-    @Given("the user is on the login page {string}")
-    public void the_user_is_on_the_login_page(String url) {
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
         driver = new ChromeDriver();
-        driver.get(url);
+        driver.get("https://www.saucedemo.com/");
         loginPage = new LoginPage(driver);
     }
 
-    @Given("the user enters a valid username {string}")
-    public void the_user_enters_a_valid_username(String username) {
+    @When("the user enters a valid username {string} and password {string}")
+    public void the_user_enters_a_valid_username_and_password(String username, String password) {
         loginPage.enterUsername(username);
-    }
-
-    @Given("the user enters a valid password {string}")
-    public void the_user_enters_a_valid_password(String password) {
         loginPage.enterPassword(password);
     }
 
-    @Given("the user enters an invalid username {string}")
-    public void the_user_enters_an_invalid_username(String username) {
-        loginPage.enterUsername(username);
-    }
-
-    @Given("the user enters an invalid password {string}")
-    public void the_user_enters_an_invalid_password(String password) {
-        loginPage.enterPassword(password);
-    }
-
-    @Given("the user leaves the username field empty")
-    public void the_user_leaves_the_username_field_empty() {
-        loginPage.enterUsername("");
-    }
-
-    @Given("the user leaves the password field empty")
-    public void the_user_leaves_the_password_field_empty() {
-        loginPage.enterPassword("");
-    }
-
-    @When("the user clicks the login button")
-    public void the_user_clicks_the_login_button() {
+    @When("clicks the login button")
+    public void clicks_the_login_button() {
         loginPage.clickLoginButton();
-    }
-
-    @When("the user clicks the login button {int} times")
-    public void the_user_clicks_the_login_button_times(int times) {
-        for (int i = 0; i < times; i++) {
-            loginPage.clickLoginButton();
-        }
     }
 
     @Then("the user should be redirected to the dashboard page")
     public void the_user_should_be_redirected_to_the_dashboard_page() {
-        // Implement dashboard page verification
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
+        driver.quit();
     }
 
-    @Then("the dashboard should display personalized content")
-    public void the_dashboard_should_display_personalized_content() {
-        // Implement personalized content verification
+    @When("the user enters an invalid username {string} or password {string}")
+    public void the_user_enters_an_invalid_username_or_password(String username, String password) {
+        loginPage.enterUsername(username);
+        loginPage.enterPassword(password);
     }
 
-    @Then("an error message should be displayed")
-    public void an_error_message_should_be_displayed() {
-        Assert.assertTrue(loginPage.getErrorMessage().length() > 0);
-    }
-
-    @Then("the error message should read {string}")
-    public void the_error_message_should_read(String expectedMessage) {
+    @Then("an error message {string} should be displayed")
+    public void an_error_message_should_be_displayed(String expectedMessage) {
         Assert.assertEquals(expectedMessage, loginPage.getErrorMessage());
+        driver.quit();
+    }
+
+    @When("the user enters a password with less than 8 characters {string}")
+    public void the_user_enters_a_password_with_less_than_8_characters(String password) {
+        loginPage.enterPassword(password);
+    }
+
+    @When("enters a valid username {string}")
+    public void enters_a_valid_username(String username) {
+        loginPage.enterUsername(username);
+    }
+
+    @Then("an error message should be displayed indicating password requirements")
+    public void an_error_message_should_be_displayed_indicating_password_requirements() {
+        Assert.assertTrue(loginPage.getErrorMessage().contains("password requirements"));
+        driver.quit();
+    }
+
+    @When("the user submits the login form with empty username or password")
+    public void the_user_submits_the_login_form_with_empty_username_or_password() {
+        loginPage.enterUsername("");
+        loginPage.enterPassword("");
+        loginPage.clickLoginButton();
+    }
+
+    @Then("an error message should be displayed indicating that both fields are required")
+    public void an_error_message_should_be_displayed_indicating_that_both_fields_are_required() {
+        Assert.assertTrue(loginPage.getErrorMessage().contains("both fields are required"));
+        driver.quit();
+    }
+
+    @When("the user enters invalid credentials 5 times with username {string} and password {string}")
+    public void the_user_enters_invalid_credentials_5_times_with_username_and_password(String username, String password) {
+        for (int i = 0; i < 5; i++) {
+            loginPage.enterUsername(username);
+            loginPage.enterPassword(password);
+            loginPage.clickLoginButton();
+        }
+    }
+
+    @Then("a message {string} should be displayed")
+    public void a_message_should_be_displayed(String expectedMessage) {
+        Assert.assertEquals(expectedMessage, loginPage.getErrorMessage());
+        driver.quit();
     }
 }
