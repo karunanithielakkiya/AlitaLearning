ce52211e4cd91248b9a429ff225cbce03cd5658c
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 12:41:28 +0530

    Update AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java

--- AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
+++ AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
@@ -1,40 +1,45 @@
-package com.epam.learn.pages;
+package com.saucedemo.automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
-    @FindBy(id = "login_credentials")
-    public WebElement loginIds;
-
-    @FindBy(className = "login_password")
-    public WebElement loginPwd;
+    WebDriver driver;
 
     @FindBy(id = "user-name")
-    private WebElement loginUser;
+    WebElement usernameField;
 
     @FindBy(id = "password")
-    private WebElement loginPassword;
+    WebElement passwordField;
 
     @FindBy(id = "login-button")
-    private WebElement loginBtn;
+    WebElement loginButton;
 
-    @FindBy(xpath = "//a[@id='logout_sidebar_link']")
-    private WebElement logoutBtn;
+    @FindBy(css = ".error-message-container")
+    WebElement errorMessageContainer;
 
-    @FindBy(xpath = "//button[@id='react-burger-menu-btn']")
-    private WebElement logoutMenu;
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
-    @FindBy(xpath = "//div[text()='Swag Labs']")
-    public WebElement homePageValidation;
+    public void enterUsername(String username) {
+        usernameField.clear();
+        usernameField.sendKeys(username);
+    }
 
-    @FindBy(xpath = "//span[text()='Products']")
-    public WebElement welcomePageText;
+    public void enterPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
 
-    Wait<WebDriver> wait = null;
+    public void clickLoginButton() {
+        loginButton.click();
+    }
 
-
+    public String getErrorMessage() {
+        return errorMessageContainer.getText();
+    }
 }
