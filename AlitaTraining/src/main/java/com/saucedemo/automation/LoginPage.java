3cb74a09f30bbf299c7da6ddeecc1847513f82c6
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 12:53:05 +0530

    Create AlitaTraining/src/main/java/com/saucedemo/automation/LoginPage.java

---
+++ AlitaTraining/src/main/java/com/saucedemo/automation/LoginPage.java
@@ -0,0 +1,49 @@
+package com.saucedemo.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    // Locators
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = ".error-message-container")
+    private WebElement errorMessage;
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Action methods
+    public void enterUsername(String username) {
+        usernameField.clear();
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
