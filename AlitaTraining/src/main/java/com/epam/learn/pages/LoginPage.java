c0eab0bab575c4542efddc69dc9eb877c09dfff9
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 18:37:43 +0530

    Create AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java

---
+++ AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package com.saucedemo.automation.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    WebDriver driver;
+
+    @FindBy(id = "user-name")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    WebElement loginButton;
+
+    @FindBy(css = ".error-message-container")
+    WebElement errorMessage;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterUsername(String username) {
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
