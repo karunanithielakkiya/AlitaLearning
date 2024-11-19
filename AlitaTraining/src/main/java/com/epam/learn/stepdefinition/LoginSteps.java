d849d59702127afaa017bc017b163b260b356863
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 11:42:17 +0530

    Update AlitaTraining/src/main/java/com/epam/learn/stepdefinition/LoginSteps.java

--- AlitaTraining/src/main/java/com/epam/learn/stepdefinition/LoginSteps.java
+++ AlitaTraining/src/main/java/com/epam/learn/stepdefinition/LoginSteps.java
@@ -11,72 +11,57 @@ public class LoginSteps {
     private WebDriver driver;
     private LoginPage loginPage;
 
-    @Given("the user is on the login page {string}")
-    public void the_user_is_on_the_login_page(String url) {
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
         driver = new ChromeDriver();
-        driver.get(url);
+        driver.get("https://www.saucedemo.com/");
         loginPage = new LoginPage(driver);
     }
 
-    @Given("the user enters a valid username {string}")
-    public void the_user_enters_a_valid_username(String username) {
-        loginPage.enterUsername(username);
+    @When("the user enters a valid username and password")
+    public void the_user_enters_a_valid_username_and_password() {
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
     }
 
-    @Given("the user enters a valid password {string}")
-    public void the_user_enters_a_valid_password(String password) {
+    @When("the user enters an invalid {string} or {string}")
+    public void the_user_enters_an_invalid_username_or_password(String username, String password) {
+        loginPage.enterUsername(username);
         loginPage.enterPassword(password);
     }
 
-    @Given("the user enters an invalid username {string}")
-    public void the_user_enters_an_invalid_username(String username) {
-        loginPage.enterUsername(username);
-    }
-
-    @Given("the user enters an invalid password {string}")
-    public void the_user_enters_an_invalid_password(String password) {
+    @When("the user enters a password {string}")
+    public void the_user_enters_a_password(String password) {
         loginPage.enterPassword(password);
     }
 
-    @Given("the user leaves the username field empty")
-    public void the_user_leaves_the_username_field_empty() {
-        loginPage.enterUsername("");
-    }
-
-    @Given("the user leaves the password field empty")
-    public void the_user_leaves_the_password_field_empty() {
-        loginPage.enterPassword("");
-    }
-
-    @When("the user clicks the login button")
-    public void the_user_clicks_the_login_button() {
+    @When("clicks the login button")
+    public void clicks_the_login_button() {
         loginPage.clickLoginButton();
-    }
-
-    @When("the user attempts to login {int} times")
-    public void the_user_attempts_to_login_times(int attempts) {
-        for (int i = 0; i < attempts; i++) {
-            loginPage.clickLoginButton();
-        }
     }
 
     @Then("the user should be redirected to the dashboard page")
     public void the_user_should_be_redirected_to_the_dashboard_page() {
-        Assert.assertEquals("http://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
     }
 
-    @Then("the dashboard should display personalized content")
-    public void the_dashboard_should_display_personalized_content() {
-        Assert.assertTrue(driver.getPageSource().contains("Welcome"));
+    @Then("an error message {string} should be displayed")
+    public void an_error_message_should_be_displayed(String expectedMessage) {
+        Assert.assertEquals(expectedMessage, loginPage.getErrorMessage());
     }
 
-    @Then("an error message should be displayed")
-    public void an_error_message_should_be_displayed() {
-        Assert.assertTrue(loginPage.getErrorMessage().length() > 0);
+    @Then("an error message should be displayed indicating that the username is required")
+    public void an_error_message_should_be_displayed_indicating_that_the_username_is_required() {
+        Assert.assertEquals("Username is required", loginPage.getErrorMessage());
     }
 
-    @Then("the error message should read {string}")
-    public void the_error_message_should_read(String expectedMessage) {
+    @Then("an error message should be displayed indicating that the password is required")
+    public void an_error_message_should_be_displayed_indicating_that_the_password is_required() {
+        Assert.assertEquals("Password is required", loginPage.getErrorMessage());
+    }
+
+    @Then("the user should be locked out with a message {string}")
+    public void the_user_should_be_locked_out_with_a_message(String expectedMessage) {
         Assert.assertEquals(expectedMessage, loginPage.getErrorMessage());
     }
 }
