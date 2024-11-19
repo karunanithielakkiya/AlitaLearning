311b4cbcdba69bdc0837f58491406dabee24fbb7
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 11:10:15 +0530

    Create AlitaTraining/src/main/java/com/epam/learn/stepdefinition/LoginSteps.java

---
+++ AlitaTraining/src/main/java/com/epam/learn/stepdefinition/LoginSteps.java
@@ -0,0 +1,83 @@
+package com.saucedemo.automation.steps;
+
+import com.saucedemo.automation.pages.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginSteps {
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @Given("the user is on the login page {string}")
+    public void the_user_is_on_the_login_page(String url) {
+        driver = new ChromeDriver();
+        driver.get(url);
+        loginPage = new LoginPage(driver);
+    }
+
+    @Given("the user enters a valid username {string}")
+    public void the_user_enters_a_valid_username(String username) {
+        loginPage.enterUsername(username);
+    }
+
+    @Given("the user enters a valid password {string}")
+    public void the_user_enters_a_valid_password(String password) {
+        loginPage.enterPassword(password);
+    }
+
+    @Given("the user enters an invalid username {string}")
+    public void the_user_enters_an_invalid_username(String username) {
+        loginPage.enterUsername(username);
+    }
+
+    @Given("the user enters an invalid password {string}")
+    public void the_user_enters_an_invalid_password(String password) {
+        loginPage.enterPassword(password);
+    }
+
+    @Given("the user leaves the username field empty")
+    public void the_user_leaves_the_username_field_empty() {
+        loginPage.enterUsername("");
+    }
+
+    @Given("the user leaves the password field empty")
+    public void the_user_leaves_the_password_field_empty() {
+        loginPage.enterPassword("");
+    }
+
+    @When("the user clicks the login button")
+    public void the_user_clicks_the_login_button() {
+        loginPage.clickLoginButton();
+    }
+
+    @When("the user clicks the login button {int} times")
+    public void the_user_clicks_the_login_button_times(int times) {
+        for (int i = 0; i < times; i++) {
+            loginPage.clickLoginButton();
+        }
+    }
+
+    @Then("the user should be redirected to the dashboard page")
+    public void the_user_should_be_redirected_to_the_dashboard_page() {
+        // Implement dashboard page verification logic
+    }
+
+    @Then("the dashboard should display personalized content")
+    public void the_dashboard_should_display_personalized_content() {
+        // Implement personalized content verification logic
+    }
+
+    @Then("an error message should be displayed")
+    public void an_error_message_should_be_displayed() {
+        Assert.assertTrue(loginPage.getErrorMessage().length() > 0);
+    }
+
+    @Then("the error message should read {string}")
+    public void the_error_message_should_read(String expectedMessage) {
+        Assert.assertEquals(expectedMessage, loginPage.getErrorMessage());
+    }
+}
