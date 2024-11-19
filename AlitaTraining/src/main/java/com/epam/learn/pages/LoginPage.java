1d7f1014431fe745bc002fededd5145d89fce646
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 11:42:16 +0530

    Update AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java

--- AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
+++ AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
@@ -1,16 +1,13 @@
-package com.epam.learn.pages;
+package com.saucedemo.automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.Assert;
 
 public class LoginPage {
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @FindBy(id = "user-name")
     private WebElement usernameField;
@@ -22,29 +19,28 @@ public class LoginPage {
     private WebElement loginButton;
 
     @FindBy(css = ".error-message-container")
-    private WebElement errorMessage;
+    private WebElement errorMessageContainer;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, 10);
         PageFactory.initElements(driver, this);
     }
 
     public void enterUsername(String username) {
-        wait.until(ExpectedConditions.visibilityOf(usernameField)).clear();
+        usernameField.clear();
         usernameField.sendKeys(username);
     }
 
     public void enterPassword(String password) {
-        wait.until(ExpectedConditions.visibilityOf(passwordField)).clear();
+        passwordField.clear();
         passwordField.sendKeys(password);
     }
 
     public void clickLoginButton() {
-        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+        loginButton.click();
     }
 
     public String getErrorMessage() {
-        return wait.until(ExpectedConditions.visibilityOf(errorMessage)).getText();
+        return errorMessageContainer.getText();
     }
 }
