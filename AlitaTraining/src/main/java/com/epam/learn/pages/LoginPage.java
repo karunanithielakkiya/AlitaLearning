cc74b9b2518103114f84b3d8fc24759cd3b630e1
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 12:33:29 +0530

    Create AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java

---
+++ AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package com.epam.learn.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    // Locators
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = ".error-message-container")
+    private WebElement errorMessage;
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Action Methods
+    public void enterUsername(String username) {
+        usernameField.clear();
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+
+    public boolean isDashboardDisplayed() {
+        // Implement logic to verify if the dashboard is displayed
+        return true; // Placeholder
+    }
+}
