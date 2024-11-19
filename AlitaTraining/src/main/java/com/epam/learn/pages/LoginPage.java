3a4f3145e9e57998bf4a9298f73ddfef3e386849
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-19 11:21:03 +0530

    Update AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java

--- AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
+++ AlitaTraining/src/main/java/com/epam/learn/pages/LoginPage.java
@@ -3,38 +3,48 @@ package com.epam.learn.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.Assert;
 
 public class LoginPage {
-
-    @FindBy(id = "login_credentials")
-    public WebElement loginIds;
-
-    @FindBy(className = "login_password")
-    public WebElement loginPwd;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     @FindBy(id = "user-name")
-    private WebElement loginUser;
+    private WebElement usernameField;
 
     @FindBy(id = "password")
-    private WebElement loginPassword;
+    private WebElement passwordField;
 
     @FindBy(id = "login-button")
-    private WebElement loginBtn;
+    private WebElement loginButton;
 
-    @FindBy(xpath = "//a[@id='logout_sidebar_link']")
-    private WebElement logoutBtn;
+    @FindBy(css = ".error-message-container")
+    private WebElement errorMessage;
 
-    @FindBy(xpath = "//button[@id='react-burger-menu-btn']")
-    private WebElement logoutMenu;
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+        PageFactory.initElements(driver, this);
+    }
 
-    @FindBy(xpath = "//div[text()='Swag Labs']")
-    public WebElement homePageValidation;
+    public void enterUsername(String username) {
+        wait.until(ExpectedConditions.visibilityOf(usernameField)).clear();
+        usernameField.sendKeys(username);
+    }
 
-    @FindBy(xpath = "//span[text()='Products']")
-    public WebElement welcomePageText;
+    public void enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOf(passwordField)).clear();
+        passwordField.sendKeys(password);
+    }
 
-    Wait<WebDriver> wait = null;
+    public void clickLoginButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+    }
 
-
+    public String getErrorMessage() {
+        return wait.until(ExpectedConditions.visibilityOf(errorMessage)).getText();
+    }
 }
