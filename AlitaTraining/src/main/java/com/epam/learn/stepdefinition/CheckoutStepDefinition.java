54550d3585a24471724fb25c149be45b547ecef9
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2024-11-26 10:35:14 +0530

    Create AlitaTraining/src/main/java/com/epam/learn/stepdefinition/CheckoutStepDefinition.java

---
+++ AlitaTraining/src/main/java/com/epam/learn/stepdefinition/CheckoutStepDefinition.java
@@ -0,0 +1,75 @@
+package com.epam.learn.stepdefinition;
+
+import com.epam.learn.pages.CheckoutPage;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+public class CheckoutStepDefinition extends BaseStep {
+
+    private CheckoutPage checkoutPage;
+
+    @Given("the user has items in their cart")
+    public void theUserHasItemsInTheirCart() {
+        // Assume items are already in the cart
+    }
+
+    @When("they click the {string} button")
+    public void theyClickTheButton(String buttonName) {
+        checkoutPage = new CheckoutPage(driver);
+        if (buttonName.equals("Checkout")) {
+            checkoutPage.clickCheckoutButton();
+        }
+    }
+
+    @Then("they should be taken to the checkout page")
+    public void theyShouldBeTakenToTheCheckoutPage() {
+        // Verify the user is on the checkout page
+        // This can be done by checking the URL or a specific element on the page
+    }
+
+    @Given("the user is on the checkout page")
+    public void theUserIsOnTheCheckoutPage() {
+        // Assume the user is already on the checkout page
+    }
+
+    @When("they enter valid shipping details {string}, {string}, {string}")
+    public void theyEnterValidShippingDetails(String firstName, String lastName, String zipCode) {
+        checkoutPage.enterShippingDetails(firstName, lastName, zipCode);
+    }
+
+    @When("click {string}")
+    public void click(String buttonName) {
+        if (buttonName.equals("Continue")) {
+            checkoutPage.clickContinue();
+        } else if (buttonName.equals("Finish")) {
+            checkoutPage.clickFinish();
+        }
+    }
+
+    @Then("the order should be successfully placed")
+    public void theOrderShouldBeSuccessfullyPlaced() {
+        // Verify the order is placed successfully
+    }
+
+    @Then("they should see a page with order summary \\(items, shipping address, payment details)")
+    public void theyShouldSeeAPageWithOrderSummaryItemsShippingAddressPaymentDetails() {
+        // Verify the order summary page is displayed
+    }
+
+    @When("they enter invalid shipping details {string}, {string}, {string}")
+    public void theyEnterInvalidShippingDetails(String firstName, String lastName, String zipCode) {
+        checkoutPage.enterShippingDetails(firstName, lastName, zipCode);
+    }
+
+    @Then("they should see an error message \"{string}\"")
+    public void theyShouldSeeAnErrorMessage(String expectedMessage) {
+        String actualMessage = checkoutPage.getErrorMessage();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+}
